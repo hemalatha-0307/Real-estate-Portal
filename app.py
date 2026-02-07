4c3b07c34bd3b44ab5b4b688e9f027ba38a15ab4
Hemalatha M <hemalatha.m.2024.it@rajalakshmi.edu.in>
2026-02-07 11:56:53 +0530

    Refactor database URI configuration
    Updated SQLALCHEMY_DATABASE_URI to use basedir for path.

--- app.py
+++ app.py
@@ -7,7 +7,9 @@ import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your-secret-key'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'site.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Uploads folder
